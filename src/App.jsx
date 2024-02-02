c6f2b275f714fff57024658aaf18355d076b14e5
vivekop0 <sarkarjii534@gamil.com>
2024-02-02 19:26:50 +0530

    added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unreachable */
+/* eslint-disable no-unused-vars */
+// /* eslint-disable react/prop-types */
+// /* eslint-disable no-unused-vars */
+// import { useState } from 'react'
+// import axios from "axios"
 
-function App() {
-  const [count, setCount] = useState(0)
+// import './App.css'
+
+// function App() {
+//   const [todos,setTodos]=useState([])
+//   useState(()=>{
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//     .then(function(response){
+//       console.log(response.data.todos)
+//       setTodos(response.data.todos)
+//     })
+//   },[])
+
+//   return (
+//     <>
+//       {todos.map((todo) => (
+//         <Todo key={todo.id} title={todo.title} description={todo.description} />
+//       ))}
+//     </>
+//   );
+// }
+
+
+
+// function Todo(props){
+//   return <div>
+//     <h1>{props.title}</h1>
+//     <h5>{props.description}</h5>
+//   </div>
+// }
+// export default App
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function App() {
+  const[id,setId]=useState(1)
+  // function handleClick(e) {
+  //   const ID =parseInt(e.target.textContent)
+  //    setId(ID)
+   
+  // }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={()=>{
+        setId(1)
+      }}>1</button>
+      <button onClick={()=>{
+        setId(2)
+      }} >2</button>
+      <button onClick={()=>{
+        setId(3)
+      }} >3</button>
+      <button onClick={()=>{
+        setId(4)
+      }}>4</button>
+      <Todo id={id} />
+    </div>
+  );
 }
 
-export default App
+function Todo({ id }) {
+  const [todos, setTodos] = useState({});
+  
+  const getTodos = async () => {
+    const res = await axios.get('https://sum-server.100xdevs.com/todo?id=' + id);
+    console.log(res.data.todo);
+    setTodos(res.data.todo);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, [ id]);
+
+  return (
+    <div>
+      <h1>{todos.title}</h1>
+      <h4>{todos.description}</h4>
+    </div>
+  );
+}
